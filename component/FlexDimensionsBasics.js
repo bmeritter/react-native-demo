b763734598205c9687dc5cbf41d584e9ce0932b1
Qiqi Zhao <qiqzhao@thoughtworks.com>
2017-10-23 21:50:45 +0800

    Test other attribute of FlexDimensionsBasics

--- component/FlexDimensionsBasics.js
+++ component/FlexDimensionsBasics.js
@@ -4,10 +4,14 @@ import {View} from 'react-native';
 export default class FlexDimensionsBasics extends Component {
     render() {
         return (
-            <View style={{flex: 1}}>
-                <View style={{flex: 1, backgroundColor: 'powderblue'}}></View>
-                <View style={{flex: 2, backgroundColor: 'skyblue'}}></View>
-                <View style={{flex: 3, backgroundColor: 'steelblue'}}></View>
+            <View style={{
+                flex: 1,
+                flexDirection: 'column',
+                justifyContent:'center',
+                alignItems: 'center'}}>
+                <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}}></View>
+                <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}}></View>
+                <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}}></View>
             </View>
         );
     }
