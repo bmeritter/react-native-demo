58961fad39e589ea8311400e2b570dd366eb1ccc
Qiqi Zhao <qiqzhao@thoughtworks.com>
2017-10-23 22:29:25 +0800

    Demo for TextInput

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import {AppRegistry} from 'react-native';
-import FlexDimensionsBasics from "./component/FlexDimensionsBasics";
+import PizzaTranslator from "./component/PizzaTranslator";
 
-AppRegistry.registerComponent('hello', () => FlexDimensionsBasics);
+AppRegistry.registerComponent('hello', () => PizzaTranslator);
